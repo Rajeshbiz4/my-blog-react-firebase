1667ca58f776a05cbe0d060661aca32ccf690305
rajeshbiz4 <rajesh.pandhare181@gmail.com>
2025-01-12 00:43:29 +0530

    minor changes - tital, logo and view page alignment

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,34 +1,43 @@
 import React from 'react';
 import './header.css';
-import logo from "./../../../download.jpeg" 
+import logo from "./../../../blog.avif" 
 // You can create a separate CSS file for styling
 
 const Header = ({ onLogout }) => {
   return (
     <header className="header">
+      <a href="/">
       <div className="logo">
         <img src={logo} alt="Logo" /> {/* Update this path */}
       </div>
+      </a>
       <nav className="nav">
         <ul>
+          {/* <li>
+            <a href="/">Home</a>
+          </li> */}
           <li>
-            <a href="/home">Home</a>
+            <a href="/">Health</a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a href="/">Wealth</a>
           </li>
           <li>
-            <a href="/services">Services</a>
+            <a href="/">Investment</a>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <a href="/">Latest news</a>
+          </li>
+
+          <li>
+            <a href="/">Govt scheme</a>
           </li>
         </ul>
       </nav>
-      <div className="user-menu">
-        <a href="/profile">Profile</a>
+      {/* <div className="user-menu">
+        <a href="/">Profile</a>
         <button onClick={onLogout}>Logout</button>
-      </div>
+      </div> */}
     </header>
   );
 };
